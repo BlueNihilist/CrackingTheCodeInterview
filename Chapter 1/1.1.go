f80c5d3669d277acc74d65fec9564b7657192dd0
IvanovM <IvanovM@fizpribor.ru>
2023-01-16 16:08:28 +0300

    Problem 1.1

---
+++ Chapter 1/1.1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
+func isUnique(str string) bool {
+	s := strings.ToLower(str)
+	m := make(map[rune]int)
+	for _, i := range s {
+		_, err := m[i]
+		if (err == false) && (strings.Contains(letters, string(i))) {
+			m[i] = 1
+		} else if err == true {
+			return false
+		}
+
+	}
+	return true
+}
+
+func main() {
+	testCases := []string{"Hello!", "Helo!", "Helo!!!!", "HelLo!", "I don't ev kw ha  bu"}
+	for _, k := range testCases {
+		fmt.Printf("isUnique(\"%s\") = %t\n", k, isUnique(k))
+	}
+}
